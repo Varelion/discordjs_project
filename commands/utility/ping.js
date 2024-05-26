85429ad0b311d7814506f9b912486d7e63b1c7d1
Varelion <99684831+Varelion@users.noreply.github.com>
2024-05-26 14:06:16 -0400

    Update utility/ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,5 +1,5 @@
 //  creating a ping.js file in the commands/utility folder for your first command. Inside this file, you're going to define and export two items.
-
+const wait = require('node:timers/promises').setTimeout;
 // The data property, which will provide the command definition shown above for registering to Discord.
 // The execute method, which will contain the functionality to run from our event handler when the command is used.
 const { SlashCommandBuilder } = require('discord.js');
@@ -11,7 +11,11 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply('Pong!'); // You have three seconds to establish the first reply
+		const ping = interaction.client.ws;
+		console.log(ping)
+		const botLatency = Date.now() - interaction.createdTimestamp;
+		await interaction.editReply(`Pong again!\nBot latency: ${botLatency}ms`);
 	},
 };
 
